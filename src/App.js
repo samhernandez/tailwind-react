ea04a8083ffc06821cdbd0b5d02901e0ffc2723b
Sam Hernandez <sam.a.hernandez@gmail.com>
2020-11-28 18:26:14 -0600

    react-create-app with tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './assets/tailwind.output.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-pink-700">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
